719a312ce30d8c8dcffde1f8d041fa7423ecd8c0
Jasper Schwager <jasper_schwager@proton.me>
2023-07-09 21:51:01 +0200

    Weird bugs

--- entity-component-system/src/main/java/entity/component/system/components/UserInputSpaceShipComponent.java
+++ entity-component-system/src/main/java/entity/component/system/components/UserInputSpaceShipComponent.java
@@ -1,1 +1,86 @@
-;
+package entity.component.system.components;
+
+import com.badlogic.gdx.Input;
+
+import java.security.Key;
+
+public enum UserInputSpaceShipComponent {
+	PLAYER_ONE(Input.Keys.W, Input.Keys.S, Input.Keys.D, Input.Keys.A, Input.Keys.E, Input.Keys.Q,
+			Input.Keys.ALT_LEFT), PLAYER_TWO(Input.Keys.I, Input.Keys.K, Input.Keys.L, Input.Keys.J, Input.Keys.O,
+			Input.Keys.U, Input.Keys.SPACE);
+	private int forwards;
+	private int backwards;
+	private int left;
+	private int right;
+	private int spinCounterClockwise;
+	private int spinClockwise;
+	private int shootPrimaryWeapons;
+
+	UserInputSpaceShipComponent (final int forwards, final int backwards, final int right, final int left,
+								 final int spinClockwise, final int spinCounterClockwise,
+								 final int shootPrimaryWeapons) {
+		this.forwards = forwards;
+		this.backwards = backwards;
+		this.left = left;
+		this.right = right;
+		this.spinCounterClockwise = spinCounterClockwise;
+		this.spinClockwise = spinClockwise;
+		this.shootPrimaryWeapons = shootPrimaryWeapons;
+	}
+
+	public int getShootPrimaryWeapons () {
+		return shootPrimaryWeapons;
+	}
+
+	public void setShootPrimaryWeapons (final int shootPrimaryWeapons) {
+		this.shootPrimaryWeapons = shootPrimaryWeapons;
+	}
+
+	public int getForwards () {
+		return forwards;
+	}
+
+	public void setForwards (final int forwards) {
+		this.forwards = forwards;
+	}
+
+	public int getBackwards () {
+		return backwards;
+	}
+
+	public void setBackwards (final int backwards) {
+		this.backwards = backwards;
+	}
+
+	public int getLeft () {
+		return left;
+	}
+
+	public void setLeft (final int left) {
+		this.left = left;
+	}
+
+	public int getRight () {
+		return right;
+	}
+
+	public void setRight (final int right) {
+		this.right = right;
+	}
+
+	public int getSpinCounterClockwise () {
+		return spinCounterClockwise;
+	}
+
+	public void setSpinCounterClockwise (final int spinCounterClockwise) {
+		this.spinCounterClockwise = spinCounterClockwise;
+	}
+
+	public int getSpinClockwise () {
+		return spinClockwise;
+	}
+
+	public void setSpinClockwise (final int spinClockwise) {
+		this.spinClockwise = spinClockwise;
+	}
+}
